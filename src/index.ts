10633eebe7c867603804c879c4b73828ec2f370d
Krish Garg <krishgarg6306@gmail.com>
2022-02-02 01:46:47 +0530

    ✨ edited for common use

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 const inProd = process.env.NODE_ENV === "production";
 
-const inDetaProd = process.env.DETA_RUNTIME;
-
 // Registering controllers.
 Object.values(controllers).forEach(({ route, router }) => {
   app.use(route, router);
@@ -17,12 +15,6 @@ app.get("/", (req, res) => {
   return res.send("Hello World");
 });
 
-// It wouldn't work if you start to listen in Deta.
-if (!inDetaProd) {
-  app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
-  });
-}
-
-// Deta instead asks for this.
-export = app;
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
